13a798e16e3efe6db1f5ce0e6f71a750bfaa12fd
wjddn1029 <wjddn1029@kakao.com>
2022-06-08 11:54:25 +0900

    [Programmers] 테스트케이스용 전역변수 제거 및 변수명 변경

--- Programmers/Programmers_로또의최고순위와최저순위_J.py
+++ Programmers/Programmers_로또의최고순위와최저순위_J.py
@@ -4,20 +4,16 @@
 # [45, 4, 35, 20, 3, 9]	[20, 9, 3, 45, 4, 35]	[1, 1]
 
 
-lottos = [44, 1, 0, 0, 31, 25]
-win_nums = [31, 10, 45, 1, 6, 19]
-a = {6: 1, 5: 2, 4: 3, 3: 4, 2: 5, 1: 6, 0: 6}
-
 def solution(lottos, win_nums):
-    max = 0
-    min = 0
+    max_num = 0
+    min_num = 0
     rank = {6: 1, 5: 2, 4: 3, 3: 4, 2: 5, 1: 6, 0: 6}
     for i in lottos:
         if i == 0:
-            max += 1
+            max_num += 1
         else:
             if i in win_nums:
-                max += 1
-                min += 1
+                max_num += 1
+                min_num += 1
 
-    return [rank.get(max), rank.get(min)]
+    return [rank.get(max_num), rank.get(min_num)]
