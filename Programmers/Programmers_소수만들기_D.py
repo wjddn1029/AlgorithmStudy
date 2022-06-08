3e8660f59014f6fe38e1f563022e4f4ba3030206
parkdohyun <freesin94@gmail.com>
2022-06-08 10:42:53 +0900

    [programmers] 소수만들기 오류 수정

--- Programmers/Programmers_소수만들기_D.py
+++ Programmers/Programmers_소수만들기_D.py
@@ -1,32 +1,22 @@
-#https://programmers.co.kr/learn/courses/30/lessons/12977
-from itertools import combinations, permutations
+from itertools import combinations
 
-# def solution(nums):
-#     answer = list(combinations(nums, 3))
-#     for i in
-#     return a
-#
-# print(solution([1,2,3,4]))
-nums = [1,2,3,4]
-answer = list(combinations(nums, 3))
-a = []
-for i in answer:
-    a.append(i[0] + i[1] + i[2])
-a = set(a)
-a = list(a)
-print(a)
+def solution(nums):
+    answer = list(combinations(nums, 3))
+    a = []
+    for i in answer:
+        a.append(i[0] + i[1] + i[2])
+
+    result = 0
+    for i in a:
+        if isPrimeNumber(i):
+            result += 1
+
+    return result
+
+
 def isPrimeNumber(a):
     flag = True
     for i in range(2, a):
         if a % i == 0:
             return False
     return flag
-
-result = 0
-for i in a:
-    print(isPrimeNumber(i))
-    if not isPrimeNumber(i):
-        result += 1
-
-
-print(result)
