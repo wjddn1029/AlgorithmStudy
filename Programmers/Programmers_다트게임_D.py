0a767c53719da9bbd9b75133d90c3b0c38bccba1
parkdohyun <freesin94@gmail.com>
2022-06-11 21:02:09 +0900

    [programmers] 다트게임 풀이

--- Programmers/Programmers_다트게임_D.py
+++ Programmers/Programmers_다트게임_D.py
@@ -1,3 +1,34 @@
-#https://programmers.co.kr/%C3%A5/courses/30/lessons/77485?language=python3
+#https://programmers.co.kr/learn/courses/30/lessons/17682?language=python3
+
+def solution(dartResult):
+    temp = ''
+    answer = []
+    for i in dartResult:
+        if i.isnumeric():
+            temp += i
+        if i == 'S':
+            answer.append(int(temp))
+            temp = ''
+        elif i == 'D':
+            answer.append(pow(int(temp), 2))
+            temp = ''
+        elif i == 'T':
+            answer.append(pow(int(temp), 3))
+            temp = ''
+        elif i == '#':
+            answer[-1] *= -1
+        elif i == '*':
+            if len(answer) > 1:
+                answer[-1] *= 2
+                answer[-2] *= 2
+            else:
+                answer[-1] *= 2
+    ans = sum(answer)
+
+    return ans
+dart = '1D2S3T*'
+
+print(solution(dart))
 
 
+# 전에 같이 풀었는데 같은 부분에서 같은 실수 했었음
