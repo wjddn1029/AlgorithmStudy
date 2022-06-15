62ae92c45fc18af1558cd6e30a99c6f8aa272f81
wjddn1029 <wjddn1029@kakao.com>
2022-06-15 21:26:12 +0900

    [Baekjoon] 단어정렬 풀이 수정(문제안푼줄알고 재풀이)

--- Sort/Baekjoon_단어정렬_J.py
+++ Sort/Baekjoon_단어정렬_J.py
@@ -1,17 +1,21 @@
-import sys
+#https://www.acmicpc.net/problem/1181
 
+import sys
 n = int(sys.stdin.readline())
-lst = []
+word = []
 
 for i in range(n):
-    lst.append(sys.stdin.readline().strip())
-set_lst = set(lst)
-lst = list(set_lst)
-lst.sort()
-lst.sort(key=len)
+    word.append(sys.stdin.readline().strip())
+set_word = list(set(word))      # 중복제거
+set_word.sort()                 # 알파벳순 정렬
+set_word.sort(key=len)          # 길이정렬
 
-for i in lst:
+for i in set_word:
     print(i)
+
+
+    
+
 
 # but
 # i
