72faf8166bdaa7363d9ae02f652a4ebeac917735
parkdohyun <freesin94@gmail.com>
2022-06-22 22:02:26 +0900

    [programmers] 비밀지도 문제풀이 추가

---
+++ Programmers/LV_1/Programmers_비밀지도_D.py
@@ -0,0 +1,9 @@
+#https://programmers.co.kr/learn/courses/30/lessons/17681
+
+def solution(n, arr1, arr2):
+    ans = [format(arr1[i] | arr2[i], '#b')[2:].rjust(n, '0').replace('1','#').replace('0',' ') for i in range(n)]
+    return ans
+
+
+print(solution(6, [46, 33, 33 ,22, 31, 50],[27 ,56, 19, 14, 14, 10]))
+
