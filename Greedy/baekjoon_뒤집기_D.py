54a7c8bb77e9297c471fc9b4910f956493d9b35b
parkdohyun <freesin94@gmail.com>
2022-06-23 22:12:01 +0900

    [Greedy] 백준_뒤집기 문제 풀이

---
+++ Greedy/baekjoon_뒤집기_D.py
@@ -0,0 +1,18 @@
+data = input()
+
+count_one = 0
+count_zero = 0
+
+if data[0] == '0':
+    count_one += 1
+else:
+    count_zero += 1
+
+for i in range(len(data) - 1):
+    if data[i] != data[i+1]:
+        if data[i+1] == '0':
+            count_one += 1
+        else:
+            count_zero += 1
+
+print(min(count_zero, count_one))
