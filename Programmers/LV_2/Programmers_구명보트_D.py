3d0398cd2a9c2a4c0d0bc1b29ce3f8aa9bd9b531
parkdohyun <freesin94@gmail.com>
2022-06-28 20:03:28 +0900

    [programmers] 구명보트 효율성 통과 X

--- Programmers/LV_2/Programmers_구명보트_D.py
+++ Programmers/LV_2/Programmers_구명보트_D.py
@@ -1,19 +1,20 @@
 #https://programmers.co.kr/learn/courses/30/lessons/42885?language=python3
 from itertools import combinations
+from collections import deque
 
 def solution(people, limit):
     weights = list(combinations(people, 2))
-    tmp = []
+    p = deque(people.sort())
+    answer = 0
     for i in weights:
         if limit >= sum(i):
-            if i[0] in people:
-                people.remove(i[0])
-            if i[1] in people:
-                people.remove(i[1])
+            if i[0] in p and i[1] in p:
+                p.pop()
+                p.pop()
+                answer += 1
 
-
-    answer = 0
-    return people
+    answer += len(people)
+    return answer
 
 
 print(solution([70, 50, 80, 50], 100))
