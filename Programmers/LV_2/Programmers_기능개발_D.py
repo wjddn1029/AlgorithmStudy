a67563cce0dcbd2e8270d51b4cfb045c5b93d1f4
parkdohyun <freesin94@gmail.com>
2022-06-29 21:36:48 +0900

    [programmers] 기능개발 push

---
+++ Programmers/LV_2/Programmers_기능개발_D.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    days = 0
+    count = 0
+
+    q = deque(progresses)
+    s = deque(speeds)
+
+    while q:
+        if q[0] + days * s[0] >= 100:
+            q.popleft()
+            s.popleft()
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            days += 1
+    answer.append(count)
+    return answer
