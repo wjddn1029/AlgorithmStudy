7093aad8822de543acb5f5366f39d78a6c715c4e
wjddn1029 <wjddn1029@kakao.com>
2022-07-05 23:42:40 +0900

    [programmers] 큰수만들기 2차풀이 push

--- Programmers/LV_2/Programmers_큰수만들기_J.py
+++ Programmers/LV_2/Programmers_큰수만들기_J.py
@@ -12,3 +12,24 @@ def solution(number, k):
 
 # 1차풀이 결과 타임아웃 => 저번 구명보트 문제처럼 스택 or 큐 사용 예정
 
+
+# 2차풀이
+def solution(number, k):
+    answer = []
+
+    for i in number:
+        if len(answer) == 0:
+            answer.append(i)
+            continue
+        if k > 0:
+            while answer[-1] < i:
+                answer.pop()
+                k -= 1
+                if len(answer) == 0 or k <= 0:
+                    break
+        answer.append(i)
+
+    answer = answer[:-k] if k > 0 else answer
+    return ''.join(answer)
+
+
