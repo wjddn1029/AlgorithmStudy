0a5cebaa7c66262f2e65f12a2b783418c767481e
parkdohyun <freesin94@gmail.com>
2022-07-05 11:35:11 +0900

    [Programmers] 큰수만들기 push (시간초과)

---
+++ Programmers/LV_2/Programmers_큰수만들기_D.py
@@ -0,0 +1,12 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/42883
+from itertools import permutations
+
+def solution(number, k):
+    answer = ''
+    return answer
+
+# 입출력 예
+# number	k	return
+# "1924"	2	"94"
+# "1231234"	3	"3234"
+# "4177252841"	4	"775841"
