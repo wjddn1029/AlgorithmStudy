dbc25905889c5b90d6916744f4d072f776609b45
parkdohyun <freesin94@gmail.com>
2022-07-06 22:05:18 +0900

    [DFS] 음려수 얼려먹기

---
+++ DFS/DFS_음료수얼려먹기_D.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+
+def dfs(x, y):
+    # 주어진 범위를 벗어나는 경우에는 즉시 종료
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j):
+            result += 1
+
+print(result)
+
