8f2b5be96eb5ec99312d87b75124d6880ece6f3f
parkdohyun <freesin94@gmail.com>
2022-07-27 22:05:55 +0900

    [programmers] 체육복 문제풀이 push

---
+++ Programmers/LV_1/Programmers_체육복_D.py
@@ -0,0 +1,33 @@
+def solution(n, lost, reserve):
+    set_reserve = set(reserve) - set(lost)
+    set_lost = set(lost) - set(reserve)
+    for i in set_reserve:
+        if i-1 in set_lost:
+            set_lost.remove(i-1)
+        elif i+1 in set_lost:
+            set_lost.remove(i+1)
+    return n - len(set_lost)
+
+
+def solution2(n, lost, reserve):
+    for i in reserve:
+        if i in lost:
+            lost.remove(i)
+        elif i - 1 in lost and i != 1:
+            lost.remove(i-1)
+        elif i + 1 in lost and i != 30:
+            lost.remove(i + 1)
+
+    answer = n - len(lost)
+
+    return answer
+
+
+
+
+
+# n	lost	reserve	    return
+# 5	[2, 4]	[1, 3, 5]	5
+# 5	[2, 4]	[3]	        4
+# 3	[3]	    [1]	        2
+
