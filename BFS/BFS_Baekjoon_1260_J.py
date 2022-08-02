10b24e560788ff17aa0df9bea060882d29e02559
wjddn1029 <wjddn1029@kakao.com>
2022-08-02 15:04:49 +0900

    [BFS] baekjoon_1260 문제풀이 push

---
+++ BFS/BFS_Baekjoon_1260_J.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+
+
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visit_list[v] = 1
+    while q:
+        v = q.popleft()
+        print(v, end=" ")
+        for i in range(1, n + 1):
+            if visit_list[i] == 0 and graph[v][i] == 1:
+                q.append(i)
+                visit_list[i] = 1
+
+
+n, m, v = map(int, sys.stdin.readline().split())
+
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+visit_list = [0] * (n + 1)
+visit_list2 = [0] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a][b] = graph[b][a] = 1
+
+bfs(v)
