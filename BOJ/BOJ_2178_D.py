9a18b5f62b44e08a17642c4ff1b48cf55914ba6a
parkdohyun <freesin94@gmail.com>
2022-08-09 23:53:42 +0900

    [boj] 미로탐색 문제 풀이 푸쉬

--- BOJ/BOJ_2178_D.py
+++ BOJ/BOJ_2178_D.py
@@ -1,16 +1,45 @@
 from collections import deque
 
+# n, m 공백으로 구분하여 입력 받기
+n, m = map(int, input().split())
 
-def bfs(graph, start, visited):
-    #시작 방문노드 큐 추가 및 방문처리
-    queue = deque([start])
-    visited[start] = 1
+# 2차원 리스트 맵 정보 입력
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+
+# 이동할 네 방향 정의 (상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    # 큐가 빌 때 까지 반복
     while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-        for i in graph[v]:
-            if visited[i] != 1:
-                queue.append(i)
-                visited[i] = 1
+        x, y = queue.popleft()
+        # 현재 위치에서 네 방향으로 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-#https://www.acmicpc.net/problem/2178
+            # 미로 찾기 공간을 벗어난 경우 무시
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue    # python continue 만나면 for문 내 실행 코드 건너뜀
+            # 이동할 수 없는 칸인 경우 무시
+            if graph[nx][ny] == 0:
+                continue
+
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    # 가장 오른쪽 아래까지의 최단 거리 반환
+    return graph[n-1][m-1]
+print(bfs(0,0))
+
+
