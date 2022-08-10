ac8b78a4edf9a76f088d65e00352b13788a71ce1
wjddn1029 <wjddn1029@kakao.com>
2022-08-10 18:48:42 +0900

    [DFS/BFS] baekjoon 불필요한 소스 제거

--- BFS/BFS_Baekjoon_1260_J.py
+++ BFS/BFS_Baekjoon_1260_J.py
@@ -19,7 +19,6 @@ n, m, v = map(int, sys.stdin.readline().split())
 
 graph = [[0] * (n + 1) for _ in range(n + 1)]
 visit_list = [0] * (n + 1)
-visit_list2 = [0] * (n + 1)
 
 for _ in range(m):
     a, b = map(int, sys.stdin.readline().split())
--- DFS/DFS_Baekjoon_1260_J.py
+++ DFS/DFS_Baekjoon_1260_J.py
@@ -2,10 +2,10 @@ import sys
 
 
 def dfs(v):
-    visit_list2[v] = 1
+    visit_list[v] = 1
     print(v, end=" ")
     for i in range(1, n + 1):
-        if visit_list2[i] == 0 and graph[v][i] == 1:
+        if visit_list[i] == 0 and graph[v][i] == 1:
             dfs(i)
 
 
@@ -13,10 +13,16 @@ n, m, v = map(int, sys.stdin.readline().split())
 
 graph = [[0] * (n + 1) for _ in range(n + 1)]
 visit_list = [0] * (n + 1)
-visit_list2 = [0] * (n + 1)
 
 for _ in range(m):
     a, b = map(int, sys.stdin.readline().split())
     graph[a][b] = graph[b][a] = 1
 
 dfs(v)
+
+# 4 5 1
+# 1 2
+# 1 3
+# 1 4
+# 2 4
+# 3 4
