0b8c4545d2f20cc2bbee1e3173382be4415ccecb
parkdohyun <freesin94@gmail.com>
2022-08-19 09:08:01 +0900

    [boj] 하노이의탑 조건 추가

--- BOJ/BOJ_1914_D.py
+++ BOJ/BOJ_1914_D.py
@@ -9,10 +9,11 @@ def hanoi(n, start, end, mid):
         print(start, end)
         hanoi(n - 1, mid, end, start)
 
+print(2**N - 1)
+if N <= 20:
+    hanoi(N, 1, 3, 2)
 
-print(hanoi(N, 1, 3, 2))
-
-# 하노이탑 알고리즘 3가지 규치
+# 하노이탑 알고리즘 3가지 규칙
 # 1. 원빈은 위에서 부터 꺼낼수 있다.
 # 2. 한번에 하나씩 이동해야한다.
 # 3. 항상 작은 원반이 위에 있어야 한다.
