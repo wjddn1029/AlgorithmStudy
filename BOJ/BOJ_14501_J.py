66f5a51682c0dc806f4a5a180a916211304487f7
wjddn1029 <wjddn1029@kakao.com>
2022-09-18 23:59:07 +0900

    [boj] 14501 2차풀이 push

--- BOJ/BOJ_14501_J.py
+++ BOJ/BOJ_14501_J.py
@@ -1,13 +1,14 @@
 N = int(input())
 li = [list(map(int, input().split())) for _ in range(N)]
-dp = [0 for _ in range(N+1)]
+dp = [0 for _ in range(N + 1)]
 
-for i in range(N):
-    for j in range(i + li[i][0], N+1):
-        if dp[j] < dp[i] + li[i][1]:
-            dp[j] = dp[i] + li[i][1]
+for i in range(N - 1, -1, -1):
+    if i + li[i][0] > N:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(dp[i + 1], li[i][1] + dp[i + li[i][0]])
 
-print(dp[-1])
+print(dp[0])
 
 
 # input:
