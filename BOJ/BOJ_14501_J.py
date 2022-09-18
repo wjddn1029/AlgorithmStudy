d81cac3abab51eeea3ec83ca34de223192bd6853
wjddn1029 <wjddn1029@kakao.com>
2022-09-18 23:50:01 +0900

    [boj] 14501 1차풀이 push

--- BOJ/BOJ_14501_J.py
+++ BOJ/BOJ_14501_J.py
@@ -1,3 +1,15 @@
+N = int(input())
+li = [list(map(int, input().split())) for _ in range(N)]
+dp = [0 for _ in range(N+1)]
+
+for i in range(N):
+    for j in range(i + li[i][0], N+1):
+        if dp[j] < dp[i] + li[i][1]:
+            dp[j] = dp[i] + li[i][1]
+
+print(dp[-1])
+
+
 # input:
 # 7
 # 3 10
