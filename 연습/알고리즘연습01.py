a41b66668d2de8788d1068ccc3ea827a5da51d2f
wjddn1029 <wjddn1029@kakao.com>
2022-11-16 18:17:25 +0900

    연습문제01 문제풀이 push

---
+++ 연습/알고리즘연습01.py
@@ -0,0 +1,22 @@
+def solution(S):
+    tmp = S.lower()
+
+    # b 만 있는 경우
+    if 'b' in tmp and 'a' not in tmp:
+        return True
+
+    # a 만 있는 경우
+    if 'a' in tmp and 'b' not in tmp:
+        return True
+    ans = True
+
+    # 둘다 있는 경우
+    for i, v in enumerate(tmp):
+        if v == 'b':
+            if 'a' in tmp[i:]:
+                ans = False
+
+    return ans
+
+S = 'abba'
+print(solution(S))
